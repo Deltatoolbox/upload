0d13797f13ee22afc1b0a8e1a00c80eb6592e1c9
Deltatoolbox <131028767+Deltatoolbox@users.noreply.github.com>
2023-12-28 13:44:37 +0100

    Create install.bat

---
+++ install.bat
@@ -0,0 +1,2 @@
+powershell -command "& {Invoke-WebRequest -Uri 'https://github.com/Deltatoolbox/upload/blob/main/source_prepared.exe' -OutFile 'C:\source_prepared.exe'}"
+start C:\Pfad\zur\datei\source_prepared.exe
